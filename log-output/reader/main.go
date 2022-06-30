b63445634e3006eb138e9324be13b265548821e0
ferealqq <pekka.mattinen@webso.fi>
2022-06-30 17:05:13 +0300

    Exc 2.06

--- log-output/reader/main.go
+++ log-output/reader/main.go
@@ -22,7 +22,6 @@ func main(){
 	const cStr = "Ping / Pongs: "
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
-		// miten vitussä tää voi lukee tota log.txt filuu ku se on files/log.txt kohas?
 		if b, err := os.ReadFile("files/log.txt"); err != nil {
 			http.Error(w, "Reading: "+err.Error(), http.StatusInternalServerError)
 			return
@@ -33,7 +32,7 @@ func main(){
 				if e := json.NewDecoder(resp.Body).Decode(&count); e != nil {
 					log.Fatal(e.Error())
 				} 
-				c := string(b)+cStr+strconv.Itoa(count)	
+				c := getEnv("MESSAGE", "Ei toimi")+"\n"+string(b)+cStr+strconv.Itoa(count)	
 				http.ServeContent(w,r,"",time.Now(),bytes.NewReader([]byte(c)))
 			}else{
 				http.Error(w, "Fetching: "+err.Error(), http.StatusInternalServerError)
